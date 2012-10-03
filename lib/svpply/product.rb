5ad40df46c98050f146e548fd6d7ce3b037eae48
Guille Lopez <guille@ductil.org>
2012-10-03 11:37:03 -0400

    Added missing product properties

--- lib/svpply/product.rb
+++ lib/svpply/product.rb
@@ -1,7 +1,9 @@
 module Svpply
   class Product
-    attr_reader :title, :price, :category, :gender, :image,
-        :image_height, :image_width, :id, :saves, :notes, :url, :svpply_url
+    attr_reader :id, :title, :price, :formatted_price, :currency_code,
+        :discount, :discount_code, :category, :categories, :gender, :image,
+        :image_height, :image_width, :saves, :notes, :status_id,
+        :date_created, :date_updated, :url, :svpply_url
 
     def self.products(attrs=nil)
       unless attrs.empty?
@@ -16,17 +18,25 @@ module Svpply
     end
 
     def initialize(hash)
+      @id = hash["id"]
       @title = hash["page_title"]
       @price = hash["price"]
+      @formatted_price = hash["formatted_price"]
+      @currency_code = hash["currency_code"]
+      @discount = hash["discount"]
+      @discount_code = hash["discount_code"]
       @category = hash["category"]
+      @categories = hash["categories"]
       @gender = hash["gender"]
       @image = hash["image"]
       @image_width = hash["image_width"]
       @image_height = hash["image_height"]
-      @id = hash["id"]
       @saves = hash["saves"]
       @notes = hash["notes"]
+      @status_id = hash["status_id"]
       @url = hash["page_url"]
+      @date_created = hash["date_created"]
+      @date_updated = hash["date_updated"]
       @svpply_url = "https://svpply.com/item/#{@id}"
     end
   end
